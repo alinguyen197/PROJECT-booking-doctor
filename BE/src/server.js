76a15ed65358f15c79cbed59631e1ddad055efe0
alinguyen197 <alinguyen197@gmail.com>
2024-08-01 22:04:02 +0700

    Add middleware http logger

--- BE/src/server.js
+++ BE/src/server.js
@@ -4,13 +4,16 @@ import dotenv from 'dotenv';
 import viewEngine from './config/viewEngine';
 import initWebRoutes from './routes/index';
 import connectDB from './config/connectDB';
-
+import morgan from 'morgan'
 
 let app = express();
 
 // config app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up Morgan http middleware log
+app.use(morgan('dev'));
 
 viewEngine(app)
 initWebRoutes(app)
